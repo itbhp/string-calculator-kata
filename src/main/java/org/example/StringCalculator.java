dc9663da7446b79ee076876cd04acee63c01e988
Angelo Sciarra <angelosciarra@ymail.com>
2023-02-25 20:21:40 +0000

    now we can sum one number

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,6 +3,9 @@ package org.example;
 public class StringCalculator
 {
     int add(String numbers) {
+        if(!numbers.isEmpty()){
+            return Integer.parseInt(numbers);
+        }
         return 0;
     }
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,5 +10,16 @@ public class StringCalculatorTest {
     @Test
     void empty_string_should_add_to_zero() {
         assertThat(new StringCalculator().add("")).isZero();
+    }
+
+    @ParameterizedTest(name = "input numbers {0} add to {1}")
+    @CsvSource({
+            "1, 1",
+            "2, 2",
+            "10, 10",
+            "2022, 2022",
+    })
+    void single_number_string_should_add_to_that_number(String input, int sumExpected) {
+        assertThat(new StringCalculator().add(input)).isEqualTo(sumExpected);
     }
 }
