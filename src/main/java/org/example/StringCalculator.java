68de62eb56320580e4b4edae57c3fdc81448f247
Paolo Sciarra <itbhps@gmail.com>
2023-02-26 17:08:17 +0100

    minor refactoring

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,13 +3,12 @@ package org.example;
 import static java.util.Arrays.stream;
 
 public class StringCalculator {
+
     int add(String input) {
         if (input.isEmpty()) {
             return 0;
         }
-
-        String[] numbers = input.split(",");
-        
+        var numbers = input.split(",");
         return stream(numbers)
                 .mapToInt(Integer::parseInt)
                 .sum();
