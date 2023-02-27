a236aa9378fbf3e1269491b50af8991ba60f3e8e
Paolo Sciarra <itbhps@gmail.com>
2023-02-27 08:54:04 +0100

    refactored code

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,29 +1,37 @@
 package org.example;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 import static java.util.Arrays.stream;
 
 public class StringCalculator {
 
+    record SeparatorStrategy(String suffix, Function<String, String> escape) {
+    }
+
+    private final Map<String, SeparatorStrategy> strategyMap = Map.of(
+            "//[", new SeparatorStrategy("]\n", Pattern::quote),
+            "//", new SeparatorStrategy("\n", Function.identity())
+    );
+
     int add(String input) {
         if (input.isEmpty()) {
             return 0;
         }
-        if (input.startsWith("//[")) {
-            int endIndex = input.indexOf("]\n");
-            var separator = input.substring(3, endIndex);
-            String substring = input.substring(endIndex + 2);
-            return sumNumbers(Pattern.quote(separator), substring);
-        }
-        if (input.startsWith("//")) {
-            int endIndex = input.indexOf("\n");
-            var separator = input.substring(2, endIndex);
-            String substring = input.substring(endIndex + 1);
-            return sumNumbers(separator, substring);
-        }
-        return sumNumbers("[\\n,]", input);
+        return strategyMap.entrySet().stream()
+                .filter(entry -> input.startsWith(entry.getKey()))
+                .findFirst()
+                .map(entry -> {
+                    var prefix = entry.getKey();
+                    var strategy = entry.getValue();
+                    int endIndex = input.indexOf(strategy.suffix);
+                    var separator = input.substring(prefix.length(), endIndex);
+                    String substring = input.substring(endIndex + prefix.length() - 1);
+                    return sumNumbers(strategy.escape.apply(separator), substring);
+                }).orElseGet(() -> sumNumbers("[\\n,]", input));
     }
 
     private static int sumNumbers(String separator, String input) {
@@ -32,14 +40,13 @@ public class StringCalculator {
                 .map(Integer::parseInt)
                 .toList();
         validate(parsedNumbers);
-        return  parsedNumbers.stream().mapToInt(value -> value).sum();
+        return parsedNumbers.stream().mapToInt(value -> value).sum();
     }
 
     private static void validate(List<Integer> parsedNumbers) {
         boolean containsNegative = parsedNumbers.stream().anyMatch(n -> n < 0);
-        if (containsNegative){
+        if (containsNegative) {
             throw new NegativeNumbersException(parsedNumbers);
         }
     }
-
 }
